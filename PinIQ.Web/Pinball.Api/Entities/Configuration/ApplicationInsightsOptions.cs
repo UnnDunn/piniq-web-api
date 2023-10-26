00de205124655d88e37cecb0af4cfd97183c6ae8
Uchendu Nwachuku <unndunn@hotmail.com>
2023-10-26 16:43:33 +0000

    Merged PR 133: Merge appInsights to master
    -Configure Serilog for use with App Insights
    -Remove startup.cs and move everything to program.cs
    -Add Application Insights configuration option to startup
    
    Related work items: #348

---
+++ PinIQ.Web/Pinball.Api/Entities/Configuration/ApplicationInsightsOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Pinball.Api.Entities.Configuration;
+
+public record ApplicationInsightsOptions(bool IsEnabled = false)
+{
+    public bool IsEnabled { get; set; } = IsEnabled;
+}
--- PinIQ.Web/Pinball.Api/Program.cs
+++ PinIQ.Web/Pinball.Api/Program.cs
@@ -1,70 +1,121 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Pinball.Api.Data;
+using Pinball.Api.Entities.Configuration;
+using Pinball.Api.Services.Interfaces;
+using Pinball.Api.Services.Interfaces.Impl;
+using Pinball.OpdbClient.Entities;
+using Pinball.OpdbClient.Interfaces;
+using Serilog;
+using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
 
-namespace Pinball.Api
+namespace Pinball.Api;
+
+public partial class Program
 {
-    public partial class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+        var builder = WebApplication.CreateBuilder(args);
 
-            CreateDbIfNotExists(host).Wait();
+        var loggerConfiguration = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration);
+        
+        // ApplicationInsights options
+        var appInsightsOptions = builder.Configuration.GetSection("Logging:ApplicationInsights")
+            .Get<ApplicationInsightsOptions>();
+        var appInsightsConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
+        if ((appInsightsOptions?.IsEnabled ?? false) && !string.IsNullOrEmpty(appInsightsConnectionString))
+        {
+            // configure application insights here
+            var telemetryConfig = new TelemetryConfiguration()
+                { ConnectionString = appInsightsConnectionString };
+            loggerConfiguration.WriteTo.ApplicationInsights(telemetryConfig, new TraceTelemetryConverter());
+        }
+        
+        Log.Logger = loggerConfiguration.CreateLogger();
+        builder.Host.UseSerilog();
+
+        builder.Services.AddHttpClient();
+			
+        builder.Services.AddDbContext<PinballDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("PinballDbContext"));
+        });
+			
+			
+        builder.Services.Configure<OpdbClientOptions>(builder.Configuration.GetSection("Opdb"));
+        builder.Services.AddScoped<IOpdbClient, OpdbClient.Interfaces.Impl.OpdbClient>();
+        builder.Services.AddScoped<IPinballMachineCatalogService, PinballMachineCatalogService>();
+        builder.Services.AddScoped<ITestOpdbService, TestOpdbService>();
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddMvcCore().AddApiExplorer();
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "PinIQ API", Version = "v1" });
+        });
+        
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.UseSwagger();
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "PinIQ API V1");
+        });
+
+        app.UseStaticFiles();
+        
+        
+        CreateDbIfNotExists(app).Wait();
            
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-        private static async Task UpdateDatabaseAsync(IHost host)
-        {
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-
-            try
-            {
-                var context = services.GetRequiredService<PinballDbContext>();
-                await context.Database.MigrateAsync();
-            } catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "A problem occurred updating the database schema");
-            }
-        }
-
-        private static async Task CreateDbIfNotExists(IHost host)
-        {
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            LogProcessCreatingDatabase(logger);
-
-            try
-            {
-                var context = services.GetRequiredService<PinballDbContext>();
-                await context.Database.EnsureCreatedAsync();
-            }
-            catch (Exception ex)
-            {
-                LogErrorUpdatingDatabase(logger, ex);
-            }
-        }
-
-        [LoggerMessage(EventId = 1101, Level = LogLevel.Information, Message = "Creating database")]
-        private static partial void LogProcessCreatingDatabase(ILogger<Program> logger);
-
-        [LoggerMessage(EventId = 1102, Level = LogLevel.Error, Message = "A problem occurred updating the database")]
-        private static partial void LogErrorUpdatingDatabase(ILogger<Program> logger, Exception ex);
+        app.Run();
     }
+    
+    private static async Task CreateDbIfNotExists(IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        LogProcessCreatingDatabase(logger);
+
+        try
+        {
+            var context = services.GetRequiredService<PinballDbContext>();
+            await context.Database.EnsureCreatedAsync();
+        }
+        catch (Exception ex)
+        {
+            LogErrorUpdatingDatabase(logger, ex);
+        }
+    }
+
+    [LoggerMessage(EventId = 1101, Level = LogLevel.Information, Message = "Creating database")]
+    private static partial void LogProcessCreatingDatabase(ILogger<Program> logger);
+
+    [LoggerMessage(EventId = 1102, Level = LogLevel.Error, Message = "A problem occurred updating the database")]
+    private static partial void LogErrorUpdatingDatabase(ILogger<Program> logger, Exception ex);
 }
